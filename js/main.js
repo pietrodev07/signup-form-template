719b858577424d0e2d8f02c39b1a0001b393f3c0
Pietro Lanza <pietro.dev.07@gmail.com>
2023-03-14 17:48:31 +0100

    feat: validation of password input

--- js/main.js
+++ js/main.js
@@ -43,3 +43,36 @@ tel.addEventListener('input', () =>{
     telError.textContent = '';
   }
 })
+
+password.addEventListener('input', (e) => {
+
+  if (password.validity.patternMismatch){
+
+    const currentValue = password.value;
+    const regExpCap = /[A-Z]/g;
+    const regExpDig = /[0-9]/g;
+    let result = '';
+
+    if(regExpCap.test(currentValue)) {
+      result += ''
+    } else {
+      result += 'Missing a capital letter <br>';
+    }
+
+    if(regExpDig.test(currentValue)) {
+      result += ''
+    } else {
+      result += 'Missing a number <br>';
+    }
+
+    if(currentValue.length < 8) {
+      result += `Missing ${8 - currentValue.length} chars`;
+    }
+
+    passwordError.innerHTML = result;
+
+  } else {
+    passwordError.textContent = '';
+  }
+
+});
