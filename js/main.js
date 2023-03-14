6ac1f52aa3098026083ca7c48c2ab929a7699bc0
Pietro Lanza <pietro.dev.07@gmail.com>
2023-03-14 17:41:31 +0100

    feat: add main.js variables and reference html element

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const tel = document.querySelector('#tel');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+const firstNameError = document.querySelector('#first-name-error');
+const lastNameError = document.querySelector('#last-name-error');
+const emailError = document.querySelector('#email-error');
+const telError = document.querySelector('#tel-error');
+const passwordError = document.querySelector('#password-error');
+const confirmPasswordError = document.querySelector('#confirm-password-error');
